276736163d49bc40a0466d4b4483553b7dc93447
Firas Sahli <firas.sahli@adlinktech.com>
2020-06-26 18:40:12 +0200

    update the constructor of the Topic entity to include the listener objdect
    Signed-off-by: Firas Sahli <firas.sahli@adlinktech.com>

--- tests/testTopic.py
+++ tests/testTopic.py
@@ -11,7 +11,7 @@ class TopicTest(unittest.TestCase):
         self.dp = Participant(0)
         self.name = "topic_name"
         self.type_support = self.rt.get_hello_world_simple_value_type_support()
-        self.topic = Topic(self.dp, self.name, self.type_support, None, None)
+        self.topic = Topic (self.dp, self.name, self.type_support , None, None, None)
 
     def tearDown(self):
         self.dp.rt.close()
@@ -26,8 +26,14 @@ class TopicTest(unittest.TestCase):
 
 #     def test_try_gen_class(self):
 #         get_dds_classes_from_idl('/home/firas/cyclone/cdds_python/tests/example.idl', 'HelloWorldData.Msg')
-#         #  self.assertTrue(gen_info is not None)
-
+#         
+#         #self.assertTrue(gen_info is not None)
+        
+    def test_get_name(self):
+        result_topic_name = ""
+        get_name = self.topic.name
+        self.assertEqual(get_name, self.topic.name, "Wrong topic name returned from property name")
+    
     def test_get_type_name(self):
         expected_topic_name = "HelloWorldData::Msg"
         get_name = self.topic.type_name()
